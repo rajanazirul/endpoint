fdbded7930e409d9272a3c328958b2d71ca232a7
Raja Nazirul <rajanazirul93@gmail.com>
2022-10-24 01:52:28 +0800

    add views POST

--- get_score/views.py
+++ get_score/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from .serializers import RecordSerializer
+
 # Create your views here.
+@api_view(["POST"])
+def get_score(request, *args, **kwargs):
+    instance=request.data
+    serializer = RecordSerializer(data=instance)
+    if serializer.is_valid(raise_exception=True):
+        serializer.save()
+        return Response(serializer.data)
+    return Response({}, status=400)
