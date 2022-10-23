378ab5bbf63a09ce3f98e48264c58b5e147b0519
Raja Nazirul <rajanazirul93@gmail.com>
2022-10-24 01:47:38 +0800

    add models

--- get_score/models.py
+++ get_score/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Record(models.Model):
+    user_id = models.CharField(max_length=200)
+    input_one = models.DecimalField(max_digits=15, decimal_places=2)
+    result = models.DecimalField(max_digits=15, decimal_places=2, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def calculate_result(self):
+        return "%.2f" %(float(self.input_one) * 0.8)
+
+    def save(self, *args, **kwargs):
+        self.result = self.calculate_result
+        super(Record, self).save(*args, **kwargs)
