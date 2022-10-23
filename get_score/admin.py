d65804ba9c25f90e065ca8707367167f6dba4e3d
Raja Nazirul <rajanazirul93@gmail.com>
2022-10-24 02:56:16 +0800

    add admin panel

--- get_score/admin.py
+++ get_score/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
 
+class RecordFilter(admin.ModelAdmin):
+    list_display=['user_id', 'input_one', 'result', 'created']
+    list_editable=['input_one', 'result']
+    search_fields=['user_id', 'input_one', 'result', 'created']
 # Register your models here.
+admin.site.register(models.Record, RecordFilter)
